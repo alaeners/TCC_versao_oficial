e97ae1860b513092885d5c28cc9c153e14650a45
alaeners <alaenedesousa@gmail.com>
2019-05-20 20:35:05 -0300

    métodos de CRUD de user

--- src/app/no-shared/user/user.component.ts
+++ src/app/no-shared/user/user.component.ts
@@ -10,18 +10,61 @@ export class UserComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-      var body = document.getElementsByTagName('body')[0];
-      body.classList.add('user-page');
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('user-page');
 
-      var navbar = document.getElementsByTagName('nav')[0];
-      navbar.classList.add('navbar-transparent');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.add('navbar-transparent');
   }
   ngOnDestroy() {
-      var body = document.getElementsByTagName('body')[0];
-      body.classList.remove('user-page');
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('user-page');
 
-      var navbar = document.getElementsByTagName('nav')[0];
-      navbar.classList.remove('navbar-transparent');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.remove('navbar-transparent');
   }
+
+  listAllUsers(nextPageToken) {
+    // List batch of users, 1000 at a time.
+    admin.auth().listUsers(1000, nextPageToken)
+      .then(function (listUsersResult) {
+        listUsersResult.users.forEach(function (userRecord) {
+          console.log('user', userRecord.toJSON());
+        });
+        if (listUsersResult.pageToken) {
+          // List next batch of users.
+          listAllUsers(listUsersResult.pageToken);
+        }
+      })
+      .catch(function (error) {
+        console.log('Error listing users:', error);
+      });
+  }
+
+  updateUser() {
+    admin.auth().updateUser(uid, {
+      email: 'modifiedUser@example.com',
+      password: 'newPassword'
+    })
+      .then(function (userRecord) {
+        // See the UserRecord reference doc for the contents of userRecord.
+        console.log('Successfully updated user', userRecord.toJSON());
+      })
+      .catch(function (error) {
+        console.log('Error updating user:', error);
+      });
+  }
+
+  deleteUser() {
+    admin.auth().deleteUser(uid)
+      .then(function () {
+        console.log('Successfully deleted user');
+      })
+      .catch(function (error) {
+        console.log('Error deleting user:', error);
+      });
+  }
+
+
 
 }
