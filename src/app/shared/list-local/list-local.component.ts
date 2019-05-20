ecfa4d7342d6df7cb3838048ffc8c27cdaaa4f4a
alaeners <alaenedesousa@gmail.com>
2019-05-19 23:55:45 -0300

    ajustes

--- src/app/shared/list-local/list-local.component.ts
+++ src/app/shared/list-local/list-local.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFireDatabase, } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireAction } from '@angular/fire/database';
+import { Observable, Subscription, BehaviorSubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list-local',
@@ -11,14 +13,22 @@ export class ListLocalComponent implements OnInit, OnDestroy {
   data: Date = new Date();
   focus;
   focus1;
+  items$: Observable<AngularFireAction<firebase.database.DataSnapshot>[]>;
+  size$: BehaviorSubject<string|null>;
 
-  constructor(db: AngularFireDatabase) {
-
+  constructor(private db: AngularFireDatabase) {
+    this.size$ = new BehaviorSubject(null);
+    this.items$ = this.size$.pipe(
+      switchMap(size =>
+        db.list('/items', ref =>
+          size ? ref.orderByChild('size').equalTo(size) : ref
+        ).snapshotChanges()
+      )
+    );
   }
-
-
-
-
+  filterBy(size: string|null) {
+    this.size$.next(size);
+  }
   ngOnInit() {
     const body = document.getElementsByTagName('body')[0];
     body.classList.add('list-local-page');
