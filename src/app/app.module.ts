f85ae83e19d0de994648e5e9390cbc0185bc1168
alaeners <alaenedesousa@gmail.com>
2019-05-20 20:17:48 -0300

    ajustes de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,35 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 import { NgModule } from '@angular/core';
-import { Http, HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { NgxMaskModule } from 'ngx-mask';
-import { AppRoutingModule } from './app.routing';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthService } from './services/auth.service';
+
+import { environment } from '../environments/environment';
 import { ComponentsModule } from './components/components.module';
 import { ExamplesModule } from './examples/examples.module';
+
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LoginComponent } from './shared/login/login.component';
 import { AboutComponent } from './shared/about/about.component';
 
-
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 import { RegisterLocalComponent } from './shared/register-local/register-local.component';
-
-import { DashboardComponent } from './no-shared/dashboard/dashboard.component';
 import { ListLocalComponent } from './shared/list-local/list-local.component';
 import { EvaluateLocalComponent } from './shared/evaluate-local/evaluate-local.component';
 import { DeleteLocalComponent } from './no-shared/delete-local/delete-local.component';
-import { DeleteUserComponent } from './no-shared/delete-user/delete-user.component';
-import { ListUserComponent } from './no-shared/list-user/list-user.component';
+import { UpdateLocalComponent } from './no-shared/update-local/update-local.component';
+
+import { DashboardComponent } from './no-shared/dashboard/dashboard.component';
 import { MergeLocalComponent } from './no-shared/merge-local/merge-local.component';
 import { NotifyLocalComponent } from './no-shared/notify-local/notify-local.component';
-import { RegisterUserComponent } from './no-shared/register-user/register-user.component';
-import { UpdateLocalComponent } from './no-shared/update-local/update-local.component';
-import { UpdateUserComponent } from './no-shared/update-user/update-user.component';
-//import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-
-import { AuthService } from './services/auth.service';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { UserComponent } from './no-shared/user/user.component';
 
 @NgModule({
@@ -44,20 +38,15 @@ import { UserComponent } from './no-shared/user/user.component';
         NavbarComponent,
         LoginComponent,
         AboutComponent,
-        DashboardComponent,
         RegisterLocalComponent,
         ListLocalComponent,
         EvaluateLocalComponent,
         DeleteLocalComponent,
-        DeleteUserComponent,
-        ListUserComponent,
+        UpdateLocalComponent,
+        DashboardComponent,
         MergeLocalComponent,
         NotifyLocalComponent,
-        RegisterUserComponent,
-        UpdateLocalComponent,
-        UpdateUserComponent,
         UserComponent
-
     ],
     imports: [
         BrowserAnimationsModule,
